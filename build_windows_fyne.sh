b979ef5c6db2b190e9eea47a80e49a9fa3ee30b5
gian angelo geminiani <angelo.geminiani@gmail.com>
2022-01-14 16:53:01 +0100

    no message

---
+++ build_windows_fyne.sh
@@ -0,0 +1,15 @@
+##!/bin/sh
+
+BASE="1.1"
+# Get latest build
+BUILD=$(<build_version.txt)
+chrlen=${#BUILD}
+if [ $chrlen = 0 ]
+then
+  BUILD=0
+fi
+
+echo "START BUILDING WINDOWS VERSION WITH FYNE-CROSS $BASE.$BUILD..."
+
+## windows
+fyne-cross windows -arch=amd64,386
